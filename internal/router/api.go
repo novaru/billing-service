40476ea5aa9834b9933ae39b59e2312ff9ab634f
Febriyan Ahza <69071485+novaru@users.noreply.github.com>
2025-09-14 22:29:32 +0700

    router: integrate middleware into API routes

--- internal/router/api.go
+++ internal/router/api.go
@@ -2,15 +2,26 @@ package router
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/novaru/billing-service/internal/middleware"
 )
 
 func (rt *Router) apiRoutes() chi.Router {
 	r := chi.NewRouter()
 
-	r.Route("/users", func(r chi.Router) {
-		r.Get("/", rt.handlers.User.FindAll)
-		r.Get("/{id}", rt.handlers.User.FindByID)
-		r.Post("/", rt.handlers.User.Create)
+	r.Route("/auth", func(r chi.Router) {
+		r.Post("/login", rt.handlers.User.Login)
+		r.Post("/register", rt.handlers.User.Create)
+	})
+
+	r.Group(func(r chi.Router) {
+		r.Use(middleware.AuthMiddleware(rt.config))
+		r.Use(middleware.APIKeyAuth())
+
+		r.Route("/users", func(r chi.Router) {
+			r.Get("/", rt.handlers.User.FindAll)
+			r.Get("/{id}", rt.handlers.User.FindByID)
+			r.Post("/", rt.handlers.User.Create)
+		})
 	})
 
 	return r
