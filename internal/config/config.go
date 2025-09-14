0299fa7e494dd8a99e9455d0ac3e4485ccc052c2
Febriyan Ahza <69071485+novaru@users.noreply.github.com>
2025-09-14 22:29:32 +0700

    config: load Env and JWT_SECRET from .env

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,22 +4,28 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"go.uber.org/zap"
+
 	"github.com/novaru/billing-service/pkg/logger"
 )
 
 type Config struct {
-	DatabaseURL string
+	Env         string
 	Port        string
+	DatabaseURL string
+	JWTSecret   string
 }
 
 func Load() *Config {
 	err := godotenv.Load()
 	if err != nil {
-		logger.Fatal("Error loading .env file", err)
+		logger.Fatal("Error loading .env file", zap.Error(err))
 	}
 
 	return &Config{
-		DatabaseURL: os.Getenv("DATABASE_URL"),
+		Env:         os.Getenv("ENV"),
 		Port:        os.Getenv("PORT"),
+		DatabaseURL: os.Getenv("DATABASE_URL"),
+		JWTSecret:   os.Getenv("JWT_SECRET"),
 	}
 }
