49c9e464b9b1cdfd7432cd775c29beae7b7b4756
novaru <69071485+novaru@users.noreply.github.com>
2025-09-15 13:56:24 +0700

    Add PlanHandler to handlers structure

--- internal/app/handler/handlers.go
+++ internal/app/handler/handlers.go
@@ -4,12 +4,15 @@ import "github.com/novaru/billing-service/internal/app/service"
 
 type Handlers struct {
 	User *UserHandler
+	Plan *PlanHandler
 }
 
 func New(
 	userService service.UserService,
+	planService service.PlanService,
 ) *Handlers {
 	return &Handlers{
 		User: NewUserHandler(userService),
+		Plan: NewPlanHandler(planService),
 	}
 }
